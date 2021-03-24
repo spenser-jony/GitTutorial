6607d8b9463a11fb1fd184a944c6b51badcfaf7d
Spenser <userjefferdesign@gmail.com>
2021-03-24 12:49:13 +0300

    added a button and a print

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Hey")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
