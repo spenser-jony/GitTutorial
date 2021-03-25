e915a019e4dcc0e549d097f76584171418a29c0c
Spenser <userjefferdesign@gmail.com>
2021-03-25 17:59:41 +0300

    Another commit

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Hello CS!93p")
         print("Hey")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
